a824f188592eb5f3adef9a806abeac4b59921c59
Viskadouros <vagelis252@gmail.com>
2019-05-26 21:00:56 +0300

    before cascade

--- base.sql
+++ base.sql
@@ -1,4 +1,3 @@
---DROP TABLE  member CASCADE ;
 CREATE TABLE member(
     memberid INT NOT NULL UNIQUE ,
     mfirst TEXT NOT NULL,
@@ -9,7 +8,14 @@ CREATE TABLE member(
     mbirthdate DATE,
     PRIMARY KEY (memberid)
 );
---DROP TABLE book CASCADE ;
+CREATE TABLE publisher(
+    pubname TEXT NOT NULL UNIQUE,
+    estyear INT,
+    street TEXT,
+    snumber INT,
+    postalcode INT,
+    PRIMARY KEY (pubname)
+);
 CREATE TABLE book(
     isbn INT NOT NULL UNIQUE ,
     title TEXT NOT NULL,  
@@ -17,9 +23,9 @@ CREATE TABLE book(
     numpages INT NOT NULL,
     pubname TEXT,
     PRIMARY KEY (isbn),
-    UNIQUE (isbn, pubname)
+    UNIQUE (isbn, pubname),
+    FOREIGN KEY (pubname) REFERENCES publisher(pubname)
 );
---DROP TABLE author CASCADE ;
 CREATE TABLE author(
     authid INT NOT NULL UNIQUE , 
     afirst TEXT NOT NULL,
@@ -27,30 +33,22 @@ CREATE TABLE author(
     abirthdate DATE,
     PRIMARY KEY (authid)
 );
---DROP TABLE category CASCADE ;
 CREATE TABLE category(
     categoryname TEXT NOT NULL ,
     supercategoryname TEXT NOT NULL ,
     PRIMARY KEY (categoryname),
     FOREIGN KEY (supercategoryname) REFERENCES category(categoryname)
 );
---DROP TABLE copies CASCADE ;
+
 CREATE TABLE copies(
     isbn INT NOT NULL,
     copynr INT NOT NULL,
     shelf INT,
-    PRIMARY KEY (isbn,copynr)
+    UNIQUE (isbn, copynr),
+    PRIMARY KEY (isbn,copynr),
+    FOREIGN KEY (isbn) REFERENCES book(isbn)
 );
---DROP TABLE publisher CASCADE ;
-CREATE TABLE publisher(
-    pubname TEXT NOT NULL,
-    estyear INT,
-    street TEXT,
-    snumber INT,
-    postalcode INT,
-    PRIMARY KEY (pubname)
-);
---DROP TABLE employee CASCADE ;
+
 CREATE TABLE employee(
     empid INT NOT NULL UNIQUE,
     efirst TEXT NOT NULL,
@@ -58,33 +56,30 @@ CREATE TABLE employee(
     salary INT,
     PRIMARY KEY (empid)
 );
---DROP TABLE permanent_employee CASCADE ;
 CREATE TABLE permanent_employee(
     empid INT NOT NULL UNIQUE ,
     hiringdate DATE,
     PRIMARY KEY (empid),
     FOREIGN KEY (empid) REFERENCES employee(empid)
 );
---DROP TABLE temporary_employee CASCADE ;
 CREATE TABLE temporary_employee(
     empid INT NOT NULL UNIQUE ,
     contractnr INT,
     PRIMARY KEY (empid),
     FOREIGN KEY (empid) REFERENCES employee(empid)
 );
---DROP TABLE borrows CASCADE ;
 CREATE TABLE borrows(
     memberid INT NOT NULL,
     isbn INT NOT NULL,
-    copynr INT NOT NULL,
+    copynr INT NOT NULL ,
     date_of_borrowing DATE NOT NULL,
     date_of_return DATE,
+    UNIQUE (memberid, isbn, copynr,date_of_borrowing),
     PRIMARY KEY (memberid,isbn,copynr,date_of_borrowing),
     FOREIGN KEY (memberid) REFERENCES member(memberid),
     FOREIGN KEY (isbn) REFERENCES book(isbn),
-    FOREIGN KEY (copynr) REFERENCES copies(copynr)
+    FOREIGN KEY (isbn,copynr) REFERENCES copies(isbn,copynr)
 );
---DROP TABLE belongs_to CASCADE ;
 CREATE TABLE belongs_to(
     isbn INT NOT NULL,
     categoryname TEXT NOT NULL,
@@ -92,7 +87,6 @@ CREATE TABLE belongs_to(
     FOREIGN KEY (isbn) REFERENCES book(isbn),
     FOREIGN KEY (categoryname) REFERENCES category(categoryname)
 );
---DROP TABLE reminder IF EXISTS CASCADE ;
 CREATE TABLE reminder(
     empid INT NOT NULL,
     memberid INT NOT NULL,
@@ -100,14 +94,15 @@ CREATE TABLE reminder(
     copynr INT NOT NULL,
     date_of_borrowing DATE NOT NULL,
     date_of_reminder DATE NOT NULL,
+    UNIQUE (memberid, isbn, copynr, date_of_borrowing) ,
     PRIMARY KEY (empid,memberid,isbn,copynr,date_of_borrowing,date_of_reminder),
     FOREIGN KEY (empid) REFERENCES employee(empid),
     FOREIGN KEY (memberid) REFERENCES member(memberid),
     FOREIGN KEY (isbn) REFERENCES book(isbn),
-    FOREIGN KEY (copynr) REFERENCES copies(copynr),
-    FOREIGN KEY (date_of_borrowing) REFERENCES borrows(date_of_borrowing)
+    FOREIGN KEY (memberid, isbn, copynr, date_of_borrowing) 
+    REFERENCES borrows(memberid, isbn, copynr, date_of_borrowing),
+    FOREIGN KEY (isbn,copynr) REFERENCES copies(isbn, copynr)
 );
---DROP TABLE written_by CASCADE ;
 CREATE TABLE written_by(
     isbn INT NOT NULL,
     authid INT NOT NULL,
@@ -116,5 +111,3 @@ CREATE TABLE written_by(
     FOREIGN KEY (authid) REFERENCES author(authid)
 );
 
-ALTER TABLE book ADD FOREIGN KEY (pubname) REFERENCES publisher(pubname);
-ALTER TABLE copies ADD FOREIGN KEY (isbn) REFERENCES book(isbn);
