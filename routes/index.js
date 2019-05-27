d82dd0e05f716020bfd719fb5750843ccceaad5b
mky13 <techno_manosal@windowslive.com>
2019-05-27 22:39:00 +0300

    allaze kwdiko kai onoma vasis sto app.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Library' });
+  res.render('index', { title: 'LibraryNTUA' });
 });
 
 module.exports = router;
