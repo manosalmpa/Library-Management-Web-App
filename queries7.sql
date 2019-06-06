0ecbd2ad89fa7b8af844dce8f479542e477dde5f
mky13 <techno_manosal@windowslive.com>
2019-06-06 04:04:30 +0300

    5queries

--- queries7.sql
+++ queries7.sql
@@ -24,3 +24,25 @@ FROM publisher
 FULL JOIN book ON publisher.pubname = book.pubname
 WHERE publisher.pubname IS NOT NULL
 GROUP BY publisher.pubname;
+
+--BOOKS PER PUBLISHED >0 (GROUP BY ME HAVING)
+SELECT publisher.pubname,
+       COUNT(book.isbn) AS C
+FROM publisher
+FULL JOIN book ON publisher.pubname = book.pubname
+WHERE publisher.pubname IS NOT NULL
+GROUP BY publisher.pubname
+HAVING c>5;
+
+--ORDER BY mlast
+SELECT member.mfirst,member.mlast,member.memberid
+FROM member
+ORDER BY member.mlast;
+
+--SHOW MEMBERS WITH MOBILE PHONE
+SELECT member.memberid,
+       member.mfirst,
+       member.mlast,
+       member.snumber
+FROM member
+WHERE CAST(member.snumber as TEXT) LIKE '69%';
